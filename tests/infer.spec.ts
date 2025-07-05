65fc6ee29dd0826964bfa8d75c562db71b870c6b
euriklis <exel_mmm@abv.bg>
2025-07-05 17:04:35 +0300

    Changed Udidi with udidi

--- tests/infer.spec.ts
+++ tests/infer.spec.ts
@@ -1,27 +1,29 @@
 import { test } from "bun:test";
 import { expectTypeOf } from "expect-type";
-import { Udidi } from "../src";
+import { udidi } from "../src";
+
+udidi.number().isInteger.isPositive;
 
 test("Udidi.Infer infers async function", () => {
-  const schema = Udidi.async().returns(Udidi.number());
-  type T = Udidi.Infer<typeof schema>;
+  const schema = udidi.async().returns(udidi.number());
+  type T = udidi.Infer<typeof schema>;
   expectTypeOf<T>().toEqualTypeOf<() => Promise<number>>();
 });
 
 test("Udidi.Infer infers promise", () => {
-  const schema = Udidi.promise().returns(Udidi.number());
-  type T = Udidi.Infer<typeof schema>;
+  const schema = udidi.promise().returns(udidi.number());
+  type T = udidi.Infer<typeof schema>;
   expectTypeOf<T>().toEqualTypeOf<Promise<number>>();
 });
 
 test("Udidi.Infer infers symbol", () => {
-  const schema = Udidi.symbol();
-  type T = Udidi.Infer<typeof schema>;
+  const schema = udidi.symbol();
+  type T = udidi.Infer<typeof schema>;
   expectTypeOf<T>().toEqualTypeOf<symbol>();
 });
 
 test("Udidi.Infer infers function returning string array", () => {
-  const schema = Udidi.function().returns(Udidi.array(Udidi.string()));
-  type T = Udidi.Infer<typeof schema>;
+  const schema = udidi.function().returns(udidi.array(udidi.string()));
+  type T = udidi.Infer<typeof schema>;
   expectTypeOf<T>().toEqualTypeOf<() => string[]>();
 });
