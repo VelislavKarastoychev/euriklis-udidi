5c055d6858bcb7ce0503456e97f42d9b61d1a6fc
euriklis <exel_mmm@abv.bg>
2025-07-05 20:32:10 +0300

    Implemented correctly the check type for all the cases.

--- src/Udidi/Models/checkType.ts
+++ src/Udidi/Models/checkType.ts
@@ -2,7 +2,25 @@
 /**
  * Utility function which is used in series of methods to check if a variable is of a given type or instance.
  * @param {any} value - the current "value" property.
+ * @param {string} type - the type to be checked.
  * @returns {boolean} if the "value" is instance of the "type", then returns true, otherwise returns false.
  */
-export const checkType = (value: any, type: string): boolean =>
-  Object.prototype.toString.call(value) === `[object ${type}]`;
+
+export const checkType = (value: any, type: string): boolean => {
+  switch (type) {
+    case "Integer":
+      return typeof value === "number" && Number.isInteger(value);
+    case "Float":
+      return (
+        typeof value === "number" &&
+        !Number.isNaN(value) &&
+        !Number.isInteger(value)
+      );
+    case "NaN":
+      return Number.isNaN(value);
+    case "TypedArray":
+      return ArrayBuffer.isView(value) && !(value instanceof DataView);
+    default:
+      return Object.prototype.toString.call(value) === `[object ${type}]`;
+  }
+};
