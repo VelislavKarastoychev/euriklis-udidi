aa8163aa0d7ccfe3cf9cfd5eb3e7d1068ad7ca95
euriklis <exel_mmm@abv.bg>
2025-07-17 23:00:39 +0300

    Deleted build and validate

--- src/Udidi/Models/index.ts
+++ src/Udidi/Models/index.ts
@@ -2,6 +2,3 @@
 
 export { checkType } from "./checkType";
 export { deepEqual } from "./deepEqual";
-export { KNOWN_KEYS } from "./knownKeys";
-export { validateTree } from "./validateTree";
-export { buildFromTree } from "./buildFromTree";
